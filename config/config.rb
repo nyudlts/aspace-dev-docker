970f205b9c1c76fffb0d58680f30f3e92fc76608
Donald Mennerich <don.mennerich@nyu.edu>
2019-09-13 15:20:21 -0400

    adding config

---
+++ config/config.rb
@@ -0,0 +1,6 @@
+# You can override the default configuration options by setting them here.
+# For example, something like like this if you want to use MySQL:
+
+AppConfig[:db_url] = "jdbc:mysql://mysql:3306/archivesspace?useUnicode=true&characterEncoding=UTF-8&user=archivesspace&password=archivesspace"
+
+AppConfig[:plugins] = ['nyu_marcxml_export_plugin', 'nyu_ead_export_plugin', 'nyu_sso_plugin', 'digitization_work_order_plugin']
